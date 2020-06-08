9667dd547372e8f90d09c38fa097896a51fba893
Jack Goode <jack.w.goode@icloud.com>
2020-06-08 15:47:16 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -7,8 +7,13 @@
 def happy_birthday(birthday_kids)
     birthday_kids.each do |kids_name, age|
         puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-    end
-  end
+      end
 end
 
 def age_appropriate_birthday(birthday_kids)
+    birthday_kids.each do |kids_name, age|
+        if age <= 12
+        puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    end
+  end
+end
